82e73e664dc5029c548a571e945f85b7757496d1
sbrick26 <swayambarik@gmail.com>
2020-10-06 02:44:39 -0500

    backwards method

--- poetryslam.py
+++ poetryslam.py
@@ -1,3 +1,4 @@
+line_list = []
 def get_file_lines(filename):
     file_lines = open(filename, "r").readlines()
 
@@ -6,12 +7,31 @@ def get_file_lines(filename):
     for x in range(len(file_lines)):
         poem_lines_dictionary[x] = file_lines[x]
 
-    line_list = []
+    
     for x in range(len(poem_lines_dictionary)):
         print(poem_lines_dictionary[x])
+        line_list.append(poem_lines_dictionary[x])
+    
+    return line_list
 
 
 get_file_lines("poem.txt")
+
+def lines_printed_backwards(lines_list):
+    for x in reversed(range(len(lines_list))):
+        print(str(x + 1) + " " + line_list[x])
+
+lines_printed_backwards(line_list)
+
+def lines_printed_random(lines_list):
+    counter = []
+    for x in range(len(lines_list)):
+        counter.append(x)
+    for x in reversed(range(len(lines_list))):
+        
+
+
+
 
 
 
